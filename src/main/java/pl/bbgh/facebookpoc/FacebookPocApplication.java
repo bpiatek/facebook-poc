70f6c395317c2a70cee05bc23113520010a3e3b7
B. Piatek <piatekbart@gmail.com>
2020-10-21 12:02:52 +0200

    initial commit

---
+++ src/main/java/pl/bbgh/facebookpoc/FacebookPocApplication.java
@@ -0,0 +1,12 @@
+package pl.bbgh.facebookpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FacebookPocApplication.class, args);
+  }
+}
---
+++ src/test/java/pl/bbgh/facebookpoc/FacebookPocApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.bbgh.facebookpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
